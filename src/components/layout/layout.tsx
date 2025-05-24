353b633dc8542ea9a9c54e3bc96fae1bae6c8770
Igor gomes <igor.anjos@twed.com.br>
2025-05-24 18:19:22 -0300

    Ajustando o estilo de acordo com o style guide

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,3 +1,4 @@
+import { Inter } from "next/font/google";
 import { Footer } from "../footer";
 import { Header } from "../header";
 
@@ -5,9 +6,13 @@ type TLayoutProps = {
   children: React.ReactNode;
 };
 
+const inter = Inter({ subsets: ["latin"] });
+
 export const Layout = ({ children }: TLayoutProps) => {
   return (
-    <div className="relative flex flex-col min-h-screen dark">
+    <div
+      className={`${inter.className} relative flex flex-col min-h-screen bg-gray-700`}
+    >
       <Header />
       <main className="flex-1 flex flex-col mb-12">{children}</main>
       <Footer />
